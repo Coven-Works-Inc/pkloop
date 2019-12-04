e538ff07e9e57357cc4561cbe01fa39aa2d11dce
Aibinuomo Ifemide Babatope <aib.ifemide@gmail.com>
2019-12-04 18:19:26 +0100

    dashboard UI underway

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import HeaderFooter from '../header_footer'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <HeaderFooter>
+                <div className="dashboard-header">
+                    <h2>My Transaction</h2>
+                </div>
+                <div className="dashboard-body">
+                    <div className="dashboard-menu">
+                        <p>My Transactions</p>
+                        <p className="lighter">Edit Profile</p>
+                        <p className="lighter">My Balance</p>
+                        <p className="lighter">Support</p>
+                        <p className="gray">Log Out</p>
+                    </div>
+                    <div className="transactions-content">
+                        Welcome, {'Username'}
+                    </div>
+                </div>
+            </HeaderFooter>
+        )
+    }
+}
+
+export default Dashboard;
