d3eef0ed4f47b8ec2d8f05d50e8c198502a6708e
Jephtah <jephtino@gmail.com>
2019-12-06 17:07:25 +0100

    fixes #48, send parcel ui

--- src/components/parcel/index.js
+++ src/components/parcel/index.js
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import HeaderFooter from '../header_footer'
+import './parcel.css'
 
-const Parcel = () => {
+class Parcel extends Component {
+  state = {}
+
+  submitHandler = e => {
+    e.preventDefault()
+  }
+  render () {
+    const text = 'Meaning'
+
     return (
-        <HeaderFooter>
-            <p className="empty-page">Parcel</p>
-        </HeaderFooter>
+      <HeaderFooter>
+        <div className='container'>
+          <h1>Send parcel</h1>
+          <div className='py-2 form-group'>
+            <form onSubmit={this.submitHandler}>
+              <input type='text' placeholder='City, Country' /> <br />
+              <input type='text' placeholder='City, Country' /> <br />
+              <select>
+                <option value='extra large' selected='selected'>
+                  Extra large (E.g Big box, electronics)
+                </option>
+                <option value='large'>large (E.g Laptops)</option>
+                <option value='medium'>medium (E.g drink packs)</option>
+                <option value='small'>small (E.g mobile phone)</option>
+              </select>
+              <br />
+              <select>
+                <option value='small' selected='selected'>
+                  0 - 5lbs
+                </option>
+                <option value='medium'>6 - 10lbs</option>
+                <option value='large'>11 - 15lbs</option>
+              </select>
+            </form>
+          </div>
+          <button className='btnQ'>Find Travellers</button>
+        </div>
+      </HeaderFooter>
     )
+  }
 }
 
-export default Parcel;
+export default Parcel
