e20411bf59132570647cc9c6b382166a9eeede53
Jephtah <jephtino@gmail.com>
2019-12-06 19:25:42 +0100

    list trip UI in progress

--- src/components/trip/index.js
+++ src/components/trip/index.js
@@ -1,10 +1,61 @@
 import React from 'react'
 import HeaderFooter from '../headerFooter'
 
+import './trips.css'
+
 const index = () => {
   return (
     <HeaderFooter>
-      <p>Welcome to the Trips Page</p>
+      <section id='banner'>
+        <div class='bannertext'>
+          <h2> List Your Trip </h2>
+        </div>
+      </section>
+      <section id='list_form'>
+        <div className='trip_container'>
+          <div className='trip_field'>
+            <input type='text' placeholder='From: City, Country' />
+          </div>
+          <div className='trip_field'>
+            <input type='text' placeholder=' To:   City, Country' />
+          </div>
+          <div className='trip_field'>
+            <input type='text' placeholder='Arrival: dd/mm/yyyy' />
+          </div>
+          <div className='trip_field'>
+            <input
+              type='text'
+              placeholder='Add Stopovers(stopovers will increase your chances of getting connections)                                                                                                      --'
+            />
+          </div>
+          <div className='dup'>
+            <input type='text' placeholder='Stopover 1  City, Country' />
+
+            <input type='text' placeholder='Stopover 2  City, Country' />
+          </div>
+          <div className='dup'>
+            <input type='text' placeholder='Stopover 3   City, Country' />
+
+            <input type='text' placeholder='Stopover 4   City, Country' />
+          </div>
+          <div className='trip_field'>
+            <input type='text' />
+          </div>
+          <div className='trip_field'>
+            <input type='text' />
+          </div>
+          <div className='trip_field'>
+            <input type='text' />
+          </div>
+          <div className='trip_field'>
+            <input type='text' />
+          </div>
+          <div className='trip_field'>
+            <textarea />
+          </div>
+          <button className='btnQ'>POST YOUR TRIP</button>
+        </div>
+      </section>
     </HeaderFooter>
   )
 }
