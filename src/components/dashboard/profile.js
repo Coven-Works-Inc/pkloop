ea54dc98f1c8ee9e733ee27f11e0aea1be123bd8
Aibinuomo Ifemide Babatope <aib.ifemide@gmail.com>
2020-01-11 12:28:52 +0100

    fund wallet logic

--- src/components/dashboard/profile.js
+++ src/components/dashboard/profile.js
@@ -9,42 +9,42 @@ const Profile = (props) => {
     const onChangeHandler = e => {
         setState({
             ...state,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         });
     };
     const onProfilePictureChange = e => {
-       const url =  'https://api.cloudinary.com/v1_1/dnrlun74l/image/upload';
-       console.log(e.target.files)
-       let formData = new FormData()
-       let unsignedUploadPreset = 'cn991n6e'
-       formData.append('file', e.target.files[0])
-       formData.append('upload_preset', unsignedUploadPreset);
-       formData.append("api_key", "123563544673959")
-       axios.post(url, formData, {
-        headers: { "X-Requested-With": "XMLHttpRequest" }
-       })
-       .then(res => {
-            console.log(res)
-    })
-        
+        // const url = 'https://api.cloudinary.com/v1_1/dnrlun74l/image/upload';
+        const url = 'https://api.cloudinary.com/v1_1/iodigital/image/upload';
+        console.log(e.target.files)
+        let formData = new FormData()
+        let unsignedUploadPreset = 'classify'
+        formData.append('file', e.target.files[0], e.target.files[0].name)
+        formData.append('upload_preset', unsignedUploadPreset);
+        // formData.append("api_key", "123563544673959")
+        // axios.post(url, formData, { headers: { "X-Requested-With": "XMLHttpRequest" } })
+        axios.post(url, formData, { headers: { 'Content-Type': 'application/json' } })
+            .then(res => { console.log(res) })
+            .catch(err => console.log(err))
     }
+
     const submitHandler = e => {
         e.preventDefault()
     }
+
     return (
         <div className="edit-profile">
             <div className="profile-picture">
                 <img src={ProfilePicture} alt="default profile" />
-                <input type="file" id="fileElem"  accept="image/*" onChange={onProfilePictureChange}/>
+                <input type="file" id="fileElem" accept="image/*" onChange={onProfilePictureChange} />
             </div>
             <div className="profile-form">
                 <form onSubmit={submitHandler}>
-                    <input type="text" placeholder="First Name" value={state.firstname} name='firstname' onChange={onChangeHandler}/>
-                    <input type="text" placeholder="Last Name" value={state.lastname} name="lastname" onChange={onChangeHandler}/>
-                    <input type="text" placeholder="Username" value={state.username} onChange={onChangeHandler}/>
-                    <input type="text" placeholder="City, Country" onChange={onChangeHandler}/>
-                    <input type="email" placeholder="Email Address" value={state.email} name="email" onChange={onChangeHandler}/>
-                    <input type="tel" placeholder="Phone Number" value={state.phone} name="phone" onChange={onChangeHandler}/>
+                    <input type="text" placeholder="First Name" value={state.firstname} name='firstname' onChange={onChangeHandler} />
+                    <input type="text" placeholder="Last Name" value={state.lastname} name="lastname" onChange={onChangeHandler} />
+                    <input type="text" placeholder="Username" value={state.username} onChange={onChangeHandler} />
+                    <input type="text" placeholder="City, Country" onChange={onChangeHandler} />
+                    <input type="email" placeholder="Email Address" value={state.email} name="email" onChange={onChangeHandler} />
+                    <input type="tel" placeholder="Phone Number" value={state.phone} name="phone" onChange={onChangeHandler} />
                     <button className="btnSmall">Save Changes</button>
                 </form>
             </div>
