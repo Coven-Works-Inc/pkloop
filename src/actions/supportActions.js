881e8f8c4ee4010c5507876e63c6e229e1a25add
Jephtah <jephtino@gmail.com>
2020-01-15 10:19:47 +0100

    wired up support ui and actions

--- src/actions/supportActions.js
+++ src/actions/supportActions.js
@@ -1,2 +1,20 @@
 import { BASE_URL } from '../config/constants'
-import { POST_SUPPORT } from '../actions/types';
+import { POST_SUPPORT } from '../actions/types'
+import axios from 'axios'
+
+export const submitTicket = data => async dispatch => {
+  await axios
+    .post(`${BASE_URL}/support/submit`, data)
+    .then(res => {
+      dispatch({
+        type: POST_SUPPORT,
+        payload: res.message
+      })
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err
+      })
+    })
+}
