c190b2ceb4a4e72cda7c484fabfb9df3350d6585
Jephtah <jephtino@gmail.com>
2020-01-29 11:27:45 +0100

    upload profile

--- src/components/dashboard/profile.js
+++ src/components/dashboard/profile.js
@@ -6,33 +6,30 @@ import DashboardHeader from './header'
 import HeaderFooter from '../headerFooter'
 
 const Profile = props => {
-  const [state, setState] = useState(props.user);
-  const [photo, setPhoto] = useState('');
-  const [imageName, setImageName] = useState('Set Display Picture');
+  const [state, setState] = useState(props.user)
+  const [photo, setPhoto] = useState('')
+  const [imageName, setImageName] = useState('Set Display Picture')
   const [displayType, changeDisplayType] = useState('form')
 
-  const fileInput = useRef(null);
+  const fileInput = useRef(null)
 
   const onRefClick = () => {
-    fileInput.current.click();
-  };
+    fileInput.current.click()
+  }
 
   const onChangeHandler = e => {
     setState({
       ...state,
       [e.target.name]: e.target.value
     })
-
-  };
+  }
   const onProfilePictureChange = e => {
-    setPhoto(e.target.files[0]);
-    setImageName(e.target.files[0].name);
-
-
-  };
+    setPhoto(e.target.files[0])
+    setImageName(e.target.files[0].name)
+  }
 
   const submitHandler = e => {
-    e.preventDefault();
+    e.preventDefault()
 
     //TODO: Get The values set up in state and push to the action for upload
 
@@ -47,128 +44,142 @@ const Profile = props => {
       country: state.country,
       email: state.email,
       phone: state.phone
-    };
-
+    }
 
     props.profileUpload(formData)
-  };
+  }
 
   return (
     <HeaderFooter>
       <div className='dashboard-header'>
-          <h2>
-            Edit Profile
-          </h2>
-        </div>
-      <div>
-      <DashboardHeader />
-      <div className='edit-profile'>
-      <div>
-        <p
-          className={displayType === 'form' ? 'active' : 'sidemenu'}
-          onClick={() => changeDisplayType('form')}>Update Profile</p>
-        <p
-          className={displayType === 'picture' ? 'active' : 'sidemenu'}
-          onClick={() => changeDisplayType('picture')}>Update Picture</p>
+        <h2>Edit Profile</h2>
       </div>
-      {displayType === 'picture' &&
-        <div className='profile-picture' style={{ display: 'flex', marginTop: '0.1rem' }}>
-          <input
-            style={{ display: 'none' }}
-            type='file'
-            id='fileElem'
-            accept='image/*'
-            onChange={onProfilePictureChange}
-            ref={fileInput}
-          />
-          <button
-            style={{
-              padding: '0.8rem 1rem',
-              color: '#fff',
-              borderRadius: '2px',
-              outline: 'none',
-              fontSize: '1rem',
-              backgroundColor: 'rgba(0, 189, 190)'
-            }}
-            onClick={onRefClick}>
-            Browse
-        </button>
+      <div>
+        <DashboardHeader />
+        <div className='edit-profile'>
           <div>
-            {console.log(state.image)}
-            <input style={{ padding: '0.8rem 3rem', backgroundColor: '#efefef' }} placeholder={imageName} disabled />
+            <p
+              className={displayType === 'form' ? 'active' : 'sidemenu'}
+              onClick={() => changeDisplayType('form')}
+            >
+              Update Profile
+            </p>
+            <p
+              className={displayType === 'picture' ? 'active' : 'sidemenu'}
+              onClick={() => changeDisplayType('picture')}
+            >
+              Update Picture
+            </p>
           </div>
+          {displayType === 'picture' && (
+            <div
+              className='profile-picture'
+              style={{ display: 'flex', marginTop: '0.1rem' }}
+            >
+              <input
+                style={{ display: 'none' }}
+                type='file'
+                id='fileElem'
+                accept='image/*'
+                onChange={onProfilePictureChange}
+                ref={fileInput}
+                onClick={onRefClick}
+              />
+              <button
+                style={{
+                  padding: '0.8rem 1rem',
+                  color: '#fff',
+                  borderRadius: '2px',
+                  outline: 'none',
+                  fontSize: '1rem',
+                  backgroundColor: 'rgba(0, 189, 190)'
+                }}
+                onClick={onRefClick}
+              >
+                Browse
+              </button>
+              <div>
+                {console.log(state.image)}
+                <input
+                  style={{ padding: '0.8rem 3rem', backgroundColor: '#efefef' }}
+                  placeholder={imageName}
+                  disabled
+                />
+              </div>
+              <button>Submit</button>
+            </div>
+          )}
+          {displayType === 'form' && (
+            <div className='profile-form'>
+              <form onSubmit={submitHandler}>
+                <input
+                  type='text'
+                  placeholder='first name'
+                  value={state.firstname}
+                  name='firstname'
+                  onChange={onChangeHandler}
+                />
+                <input
+                  type='text'
+                  placeholder='last name'
+                  value={state.lastname}
+                  name='lastname'
+                  onChange={onChangeHandler}
+                />
+                <input
+                  type='text'
+                  placeholder='username'
+                  value={state.username}
+                  onChange={onChangeHandler}
+                />
+                <input
+                  type='text'
+                  name='street'
+                  placeholder='street'
+                  value={state.street}
+                  onChange={onChangeHandler}
+                />
+                <input
+                  type='text'
+                  name={'city'}
+                  placeholder='city'
+                  value={state.city}
+                  onChange={onChangeHandler}
+                />
+                <input
+                  type='text'
+                  name={'state'}
+                  placeholder='state'
+                  value={state.state}
+                  onChange={onChangeHandler}
+                />
+                <input
+                  type={'text'}
+                  name={'country'}
+                  placeholder={'country'}
+                  value={state.country}
+                  onChange={onChangeHandler}
+                />
+                <input
+                  type='email'
+                  placeholder='email address'
+                  value={state.email}
+                  name='email'
+                  onChange={onChangeHandler}
+                />
+                <input
+                  type='tel'
+                  placeholder='phone number'
+                  value={state.phone}
+                  name='phone'
+                  onChange={onChangeHandler}
+                />
+                <button className='btnSmall'>Save Changes</button>
+              </form>
+            </div>
+          )}
         </div>
-      }
-      {displayType === 'form' &&
-        <div className='profile-form'>
-          <form onSubmit={submitHandler}>
-            <input
-              type='text'
-              placeholder='first name'
-              value={state.firstname}
-              name='firstname'
-              onChange={onChangeHandler}
-            />
-            <input
-              type='text'
-              placeholder='last name'
-              value={state.lastname}
-              name='lastname'
-              onChange={onChangeHandler}
-            />
-            <input
-              type='text'
-              placeholder='username'
-              value={state.username}
-              onChange={onChangeHandler}
-            />
-            <input
-              type='text'
-              name='street'
-              placeholder='street'
-              value={state.street}
-              onChange={onChangeHandler}
-            />
-            <input
-              type='text'
-              name={'city'}
-              placeholder='city'
-              value={state.city}
-              onChange={onChangeHandler}
-            />
-            <input
-              type='text'
-              name={'state'}
-              placeholder='state'
-              value={state.state}
-              onChange={onChangeHandler}
-            />
-            <input
-              type={'text'}
-              name={'country'}
-              placeholder={'country'}
-              value={state.country}
-              onChange={onChangeHandler} />
-            <input
-              type='email'
-              placeholder='email address'
-              value={state.email}
-              name='email'
-              onChange={onChangeHandler}
-            />
-            <input
-              type='tel'
-              placeholder='phone number'
-              value={state.phone}
-              name='phone'
-              onChange={onChangeHandler}
-            />
-            <button className='btnSmall' >Save Changes</button>
-          </form>
-        </div>
-      }
-    </div>
-    </div>
+      </div>
     </HeaderFooter>
   )
 }
