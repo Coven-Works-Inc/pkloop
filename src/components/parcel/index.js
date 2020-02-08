7212b2ec2e8d2c8c50eb7e13bb39626267e99f15
olalekanteeblaze <olalekanteeblaze@gmail.com>
2020-02-07 16:10:18 -0800

    removed extra box

--- src/components/parcel/index.js
+++ src/components/parcel/index.js
@@ -632,12 +632,7 @@ const Parcel = props => {
                       ></span>) : state.totalAndTip === 0? `Pay $${Number(state.totalCost).toFixed(2)} + $${(0.05 * Number(state.totalCost)).toFixed(2)}` : `Pay ${Number(state.totalAndTip).toFixed(2)} + $${(0.05 * Number(state.totalAndTip)).toFixed(2)}`}</button>
                           <button className="btnQ inverse-btnQ medium" onClick={toggleModal}>No, Change weight</button>
                         </div>
-                      )}
                       <textarea className="support_input" placeholder="Leave a message for traveler" onChange={messageChangeHandler}></textarea>
-                      <div className="button-group">
-                        <button className="btnQ medium" onClick={connectToTraveler}>Pay ${state.totalCost} + ${(0.05 * Number(state.totalCost)).toFixed(2)}</button>
-                        <button className='btnQ inverse-btnQ medium' onClick={toggleModal}>No, Change weight</button>
-                      </div>
                     </div>
                   )
 
