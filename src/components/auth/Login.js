e9e492906f82d9417c0ca65b597ec5abfe20a9eb
Jephtah <jephtino@gmail.com>
2020-02-15 02:16:46 +0100

    rewrote google and facebook logic

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -28,7 +28,7 @@ class Login extends Component {
 
   componentDidMount () {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard')
+      this.props.history.push('/dashboard/transactions')
     }
   }
 
