3b97bb20ef6395c62ebceceacbf1cc502d90f3c4
olalekanteeblaze <olalekanteeblaze@gmail.com>
2020-02-21 13:33:07 +0100

    changed to standard account

--- src/components/dashboard/balance.js
+++ src/components/dashboard/balance.js
@@ -12,7 +12,7 @@ import Connect from '../../assets/connect.png'
 
 const Balance = props => {
   // const { balance: { user: { balance } } } = props
-  const url = 'https://dashboard.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_G5xbYGir14T6X3N1BNHW6K0a3LMsrayQ&scope=read_write'
+  const url = 'https://dashboard.stripe.com/standard/oauth/authorize?response_type=code&client_id=ca_G5xbYGir14T6X3N1BNHW6K0a3LMsrayQ&scope=read_write'
   const [state, setState] = useState({
     amountMade: 0,
     modalOpen: false,
